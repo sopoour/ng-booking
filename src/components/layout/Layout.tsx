b9a5a37f70512635abf64c41adccad699dc27c91
Fio <36188360+sopoour@users.noreply.github.com>
2026-08-02 15:45:19 +0200

    setup with colours, fonts and general setup (#1)

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -4,30 +4,47 @@ import styled from 'styled-components';
 import Header from './Header';
 import Footer from './Footer';
 import Sidebar from '../Sidebar';
+import { flexColumn } from '@app/styles/mixins';
 
 const Root = styled.div`
+  position: relative;
   min-height: 100vh;
-  display: flex;
-  flex-direction: column;
+  ${flexColumn};
+  width: 100%;
+  background: url('/background.png');
+  background-repeat: repeat;
+  background-size: 1000px 1000px;
+  background-position: top left;
+  z-index: 1;
+
+  &::before {
+    content: '';
+    height: 100%;
+    position: absolute;
+    min-height: 100vh;
+    width: 100%;
+    background: #250564;
+    opacity: 0.4;
+  }
 `;
 
 const MainLayout = styled.main`
-  max-width: 1000px;
-  padding: 48px 20px;
-  flex: 1;
+  min-height: 100vh;
   width: 100%;
-  margin: 0 auto;
+  flex: 1;
+  z-index: 2;
 `;
 
 type Props = {
   children: ReactNode;
+  className?: string;
 };
 
-const Layout: FC<Props> = ({ children }) => (
+const Layout: FC<Props> = ({ children, className }) => (
   <Root>
     <Sidebar>Some content</Sidebar>
     <Header />
-    <MainLayout>{children}</MainLayout>
+    <MainLayout className={className}>{children}</MainLayout>
     <Footer />
   </Root>
 );
---
+++ src/fonts/fonts.ts
@@ -0,0 +1,29 @@
+import localFont from 'next/font/local';
+import {Leckerli_One, Manrope} from 'next/font/google'
+
+const header = localFont({
+  src: "./Null_Free.otf",
+  variable: "--font-neue",
+  display: "swap",
+  preload: true,
+});
+
+const subheader = Leckerli_One({
+  weight: ['400'],
+  style: ['normal'],
+  subsets: ['latin'],
+  fallback: ['sans-serif'],
+  display: 'block',
+  preload: true,
+})
+
+const text = Manrope({
+  weight: ['400', '600', '700'],
+  style: ['normal'],
+  subsets: ['latin'],
+  fallback: ['sans-serif'],
+  display: 'swap',
+  preload: true,
+})
+
+export default {header, text, subheader}
