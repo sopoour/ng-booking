f8d57a6cc141f5ce148f7230b77afea205b25e0e
sopoour <sophia.auer@googlemail.com>
2026-08-10 20:50:31 +0200

    cleanup

--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { css, styled } from 'styled-components';
 import { gsap } from 'gsap';
 import ScrollTrigger from 'gsap/dist/ScrollTrigger';
