c151ba8c3bf1adfdc9c19bdc7e07fc6c1143639b
sopoour <sophia.auer@googlemail.com>
2026-08-16 16:26:03 +0200

    remove head from _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,41 +4,20 @@ import Layout from '@app/components/layout/Layout';
 import { GlobalStyle } from '@app/styles/global';
 import { ThemeProvider } from 'styled-components';
 import theme from '@app/styles/theme';
-import Head from 'next/head';
 import '@mantine/core/styles.css';
 import 'react-loading-skeleton/dist/skeleton.css';
 import { MantineProvider } from '@mantine/core';
 
 const App = ({ Component, pageProps }: AppProps) => {
-  // TODO: Change description and title
-  const metaDescription = 'NG-Booking';
-  const metaTitle = 'NG-Booking';
-
   return (
-    <>
-      <Head>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"
-        />
-        <meta charSet="utf-8" />
-        <meta key="name" itemProp="name" content={metaTitle} />
-        <meta key="description" name="description" content={metaDescription} />
-        <meta key="og:title" property="og:title" content={metaTitle} />
-        <meta key="og:type" property="og:type" content="website" />
-        <meta key="og:description" property="og:description" content={metaDescription} />
-        <meta key="og:site_name" property="og:site_name" content="Sophia Auer" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <ThemeProvider theme={theme}>
-        <MantineProvider withGlobalClasses withCssVariables forceColorScheme="light">
-          <GlobalStyle />
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
-        </MantineProvider>
-      </ThemeProvider>
-    </>
+    <ThemeProvider theme={theme}>
+      <MantineProvider withGlobalClasses withCssVariables forceColorScheme="light">
+        <GlobalStyle />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </MantineProvider>
+    </ThemeProvider>
   );
 };
 
